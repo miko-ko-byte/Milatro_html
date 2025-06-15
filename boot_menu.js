491ae7c4c58cd2ca3ba06b9e535c16b8da8ecbd4
miko draws <84086718+miko-ko-byte@users.noreply.github.com>
2025-06-15 21:05:13 +0200

    initial init

---
+++ boot_menu.js
@@ -0,0 +1,6 @@
+console.log("boot_menu.js loaded");
+console.log("[boot_menu.js] Starting boot menu...");
+document.getElementsByClassName("play-area")[0].style.display = "none";
+document.getElementsByClassName("left-panel")[0].style.display = "none";
+document.getElementsByClassName("right-panel")[0].style.display = "none";
+document.getElementsByClassName("boot-menu-start-game")[0].style.display = "none";
